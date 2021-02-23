27d912b1c6b7449c51bc21699ba950646d1b52a1
Eren Pekşen <peksen.eren@gmail.com>
2021-02-24 01:04:51 +0300

    Moved constants and ig_account to another files. Added some logs.

---
+++ ig_account.py
@@ -0,0 +1,4 @@
+class IG_ACCOUNT:
+    def __init__(myobj, username, password):
+        myobj.username = username
+        myobj.password = password
--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-from re import TEMPLATE
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from datetime import datetime, timedelta
 import threading
 import time
 import random
-import os
-
-class IG_ACCOUNT:
-    def __init__(myobj, username, password):
-        myobj.username = username
-        myobj.password = password
+import constants
 
 INPUT_USERNAME = "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input"
 INPUT_PASSWORD = "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input"
@@ -18,33 +13,21 @@ BUTTON_DONTSAVE = "/html/body/div[1]/section/main/div/div/div/div/button"
 BUTTON_OPENUNFOLLOW = "//*[@id='react-root']/section/main/div/header/section/div[1]/div[2]/div/div[2]/div/span/span[1]/button"
 BUTTON_UNFOLLOW = "//*[text()='Unfollow']"
 BUTTON_FOLLOW = "//*[text()='Follow']"
-TEST_ACCOUNTS = [ IG_ACCOUNT("", "") ]
 INSTAGRAM_MAINPAGE_URL = "https://www.instagram.com/"
-ACCOUNTS = [
-    "cristiano", 
-    "arianagrande"
-]
-IDLE_FOR_PER_ACCOUNT = 5
-CONSTANT_IDLE_RANDOM_MULTIPILER_FOR_PER_ACCOUNT = 1
-IDLE_FOR_PER_ITERATE = 300
-CONSTANT_IDLE_RANDOM_MULTIPILER_FOR_PER_ITERATE = 10
-IDLE_FOR_UNHANDLED_BAN = 600
-IDLE_FOR_ELEMENT_TO_BE_FOUND = 10
 
-def startFlow(account, index):
-    print(account.username, account.password)
+def startFlow(CURRENT_ACCOUNT, index):
     DRIVER = webdriver.Chrome(ChromeDriverManager().install())
-    DRIVER.implicitly_wait(IDLE_FOR_ELEMENT_TO_BE_FOUND)
+    DRIVER.implicitly_wait(constants.IDLE_FOR_ELEMENT_TO_BE_FOUND)
     DRIVER.get(INSTAGRAM_MAINPAGE_URL)
 
     try:
         usernameInput = DRIVER.find_element_by_xpath(INPUT_USERNAME)
         usernameInput.clear()
-        usernameInput.send_keys(account.username)
+        usernameInput.send_keys(CURRENT_ACCOUNT.username)
 
         passwordInput = DRIVER.find_element_by_xpath(INPUT_PASSWORD)
         passwordInput.clear()
-        passwordInput.send_keys(account.password)
+        passwordInput.send_keys(CURRENT_ACCOUNT.password)
 
         DRIVER.find_element_by_xpath(BUTTON_LOGIN).click()
 
@@ -54,8 +37,12 @@ def startFlow(account, index):
 
         DRIVER.quit()
 
+    COUNT = 0;
+    
     while True:
-        shuffledACCOUNTS = random.shuffle(ACCOUNTS.copy())
+        shuffledACCOUNTS = constants.ACCOUNTS.copy()
+        random.shuffle(shuffledACCOUNTS)
+        
         for ACCOUNT in shuffledACCOUNTS:
 
             try:
@@ -63,7 +50,7 @@ def startFlow(account, index):
             except:
                 print(f'page opening failed for:"{INSTAGRAM_MAINPAGE_URL}{ACCOUNT}"')
             
-                time.sleep(IDLE_FOR_UNHANDLED_BAN)
+                time.sleep(constants.IDLE_FOR_UNHANDLED_BAN)
 
             try:
                 DRIVER.find_element_by_xpath(BUTTON_OPENUNFOLLOW).click()
@@ -72,18 +59,28 @@ def startFlow(account, index):
             except:
                 print(f'follow failed for:"{ACCOUNT}"')
                     
-                time.sleep(IDLE_FOR_UNHANDLED_BAN)  
+                time.sleep(constants.IDLE_FOR_UNHANDLED_BAN)  
 
             try:
                 DRIVER.find_element_by_xpath(BUTTON_FOLLOW).click()
             except:
                 print(f'follow failed for:"{ACCOUNT}"')
             
-                time.sleep(IDLE_FOR_UNHANDLED_BAN)
+                time.sleep(constants.IDLE_FOR_UNHANDLED_BAN)
 
-            time.sleep(IDLE_FOR_PER_ACCOUNT + (random.uniform(0, 1) * CONSTANT_IDLE_RANDOM_MULTIPILER_FOR_PER_ACCOUNT))
+            time.sleep(constants.IDLE_FOR_PER_ACCOUNT + (random.uniform(0, 1) * constants.CONSTANT_IDLE_RANDOM_MULTIPILER_FOR_PER_ACCOUNT))
     
-        time.sleep(IDLE_FOR_PER_ITERATE + (random.uniform(0, 1) * CONSTANT_IDLE_RANDOM_MULTIPILER_FOR_PER_ITERATE))
+        WAIT_FOR = constants.IDLE_FOR_PER_ITERATE + (random.uniform(0, 1) * constants.CONSTANT_IDLE_RANDOM_MULTIPILER_FOR_PER_ITERATE)
+        
+        COUNT += 1
+        
+        NOW = datetime.now()
+        
+        START_TIME = timedelta(seconds=WAIT_FOR)
+        
+        print(f'Iterate ended for:Thread-{index}. Total:{COUNT}. Ended at:{NOW}. Gonna Start at:{START_TIME}')
+        
+        time.sleep(WAIT_FOR)
 
-for i in range(len(TEST_ACCOUNTS)):
-    threading.Thread(target=startFlow, args=(TEST_ACCOUNTS[i], i)).start()
+for i in range(len(constants.TEST_ACCOUNTS)):
+    threading.Thread(target=startFlow, args=(constants.TEST_ACCOUNTS[i], i)).start()
