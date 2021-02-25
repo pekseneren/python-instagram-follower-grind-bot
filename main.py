d16e961d8c7e8339e7c75dcce20506bbaa5e2cdf
Eren Pekşen <peksen.eren@gmail.com>
2021-02-25 22:58:57 +0300

    Redesigned flow. Added/Fixed some log messages.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from logging import log
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime, timedelta
 import threading
@@ -15,29 +17,73 @@ BUTTON_UNFOLLOW = "//*[text()='Unfollow']"
 BUTTON_FOLLOW = "//*[text()='Follow']"
 INSTAGRAM_MAINPAGE_URL = "https://www.instagram.com/"
 
+def checkIfElementExist(self, xpath):
+    try:
+        element = self.find_element_by_xpath(xpath)
+        return True
+    except NoSuchElementException:
+        return False
+
+def login(self, account):
+    try:
+        usernameInput = self.find_element_by_xpath(INPUT_USERNAME)
+        usernameInput.clear()
+        usernameInput.send_keys(account.username)
+
+        passwordInput = self.find_element_by_xpath(INPUT_PASSWORD)
+        passwordInput.clear()
+        passwordInput.send_keys(account.password)
+
+        self.find_element_by_xpath(BUTTON_LOGIN).click()
+
+        self.find_element_by_xpath(BUTTON_DONTSAVE).click()
+
+        print(f'logged as {account.username}')
+        
+        return True
+    except:
+        input(f'login failed for {account.username}, enter something to exit. at:{datetime.now()}')
+
+        self.quit()
+
+        return False
+
+def triggerFollow(self, account, target):
+    try:
+        self.find_element_by_xpath(BUTTON_FOLLOW).click()
+
+        print(f'follow successed for:"{target}" at:{datetime.now()} current_acount:{account}')
+
+        time.sleep(constants.IDLE_FOR_COMMON_ACTIONS)
+    except:
+        print(f'follow failed for:"{target}" at:{datetime.now()} gonna wait:{constants.IDLE_FOR_UNHANDLED_BAN} seconds. current_acount:{account}')
+
+        time.sleep(constants.IDLE_FOR_UNHANDLED_BAN)
+
+def triggerUnfollow(self, account, target):
+    try:
+        self.find_element_by_xpath(BUTTON_OPENUNFOLLOW).click()
+
+        self.find_element_by_xpath(BUTTON_UNFOLLOW).click()
+
+        print(f'unfollow successed for:"{target}" at:{datetime.now()} current_account:{account}')
+        
+        time.sleep(constants.IDLE_FOR_COMMON_ACTIONS)
+    except:
+        print(f'unfollow failed for:"{target}" at:{datetime.now()} gonna wait:{constants.IDLE_FOR_UNHANDLED_BAN} seconds. current_acount:{account}')
+                    
+        time.sleep(constants.IDLE_FOR_UNHANDLED_BAN)
+
 def startFlow(CURRENT_ACCOUNT, index):
+    
+    COUNT = 0;
+
     DRIVER = webdriver.Chrome(ChromeDriverManager().install())
     DRIVER.implicitly_wait(constants.IDLE_FOR_ELEMENT_TO_BE_FOUND)
     DRIVER.get(INSTAGRAM_MAINPAGE_URL)
 
-    try:
-        usernameInput = DRIVER.find_element_by_xpath(INPUT_USERNAME)
-        usernameInput.clear()
-        usernameInput.send_keys(CURRENT_ACCOUNT.username)
-
-        passwordInput = DRIVER.find_element_by_xpath(INPUT_PASSWORD)
-        passwordInput.clear()
-        passwordInput.send_keys(CURRENT_ACCOUNT.password)
-
-        DRIVER.find_element_by_xpath(BUTTON_LOGIN).click()
-
-        DRIVER.find_element_by_xpath(BUTTON_DONTSAVE).click()
-    except:
-        input("login failed, enter something to exit.")
-
-        DRIVER.quit()
-
-    COUNT = 0;
+    if login(DRIVER, CURRENT_ACCOUNT) == False:
+        return
     
     while True:
         shuffledACCOUNTS = constants.ACCOUNTS.copy()
@@ -45,28 +91,13 @@ def startFlow(CURRENT_ACCOUNT, index):
         
         for ACCOUNT in shuffledACCOUNTS:
 
-            try:
-                DRIVER.get(INSTAGRAM_MAINPAGE_URL + ACCOUNT)
-            except:
-                print(f'page opening failed for:"{INSTAGRAM_MAINPAGE_URL}{ACCOUNT}"')
-            
-                time.sleep(constants.IDLE_FOR_UNHANDLED_BAN)
+            DRIVER.get(INSTAGRAM_MAINPAGE_URL + ACCOUNT)
 
-            try:
-                DRIVER.find_element_by_xpath(BUTTON_OPENUNFOLLOW).click()
-
-                DRIVER.find_element_by_xpath(BUTTON_UNFOLLOW).click()
-            except:
-                print(f'follow failed for:"{ACCOUNT}"')
-                    
-                time.sleep(constants.IDLE_FOR_UNHANDLED_BAN)  
-
-            try:
-                DRIVER.find_element_by_xpath(BUTTON_FOLLOW).click()
-            except:
-                print(f'follow failed for:"{ACCOUNT}"')
-            
-                time.sleep(constants.IDLE_FOR_UNHANDLED_BAN)
+            if checkIfElementExist(DRIVER, BUTTON_FOLLOW) == False:
+                triggerUnfollow(DRIVER, CURRENT_ACCOUNT.username, ACCOUNT)
+                triggerFollow(DRIVER, CURRENT_ACCOUNT.username, ACCOUNT)
+            else:
+                triggerFollow(DRIVER, CURRENT_ACCOUNT.username, ACCOUNT)
 
             time.sleep(constants.IDLE_FOR_PER_ACCOUNT + (random.uniform(0, 1) * constants.CONSTANT_IDLE_RANDOM_MULTIPILER_FOR_PER_ACCOUNT))
     
@@ -76,7 +107,7 @@ def startFlow(CURRENT_ACCOUNT, index):
         
         NOW = datetime.now()
         
-        START_TIME = timedelta(seconds=WAIT_FOR)
+        START_TIME = timedelta(seconds=WAIT_FOR) + NOW
         
         print(f'Iterate ended for:Thread-{index}. Total:{COUNT}. Ended at:{NOW}. Gonna Start at:{START_TIME}')
         
